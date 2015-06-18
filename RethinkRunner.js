8c0f55e4b1ace379bf8edc2b64ac636ef52e3657
binRick <rapidvps@gmail.com>
2015-06-18 01:09:12 -0600

    new

---
+++ RethinkRunner.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+pj = require('prettyjson');
+var r = require('rethinkdb');
+var c = require('chalk');
+
+var connection = null;
+r.connect({
+    host: 'localhost',
+    port: 28015
+}, function(err, conn) {
+    if (err) throw err;
+    connection = conn;
+
+var F = function(r){
+return true;
+return r.new_val;
+};
+    r.db(process.env.rethinkDatabase).table(process.env.rethinkTable).changes().run(connection, function(err, cursor) {
+        if (err) throw err;
+        cursor.each(function(err, row) {
+            if (err) throw err;
+//            console.log(pj.render(c.green(row.old_val.hostname)));
+//            console.log(pj.render(row.new_val.hostname));
+//            console.log(c.underline.black.bgWhite(row.new_val.id));
+console.log(pj.render(row));
+            console.log(pj.render(JSON.stringify(row.new_val).length), 'byte update');
+            //            console.log(JSON.stringify(row, null, 2));
+        });
+    });
+
+});
--- index.js
+++ index.js
@@ -3,11 +3,10 @@ var pj = require('prettyjson');
 
 var type = thinky.type;
 
-// Create a model - the table is automatically created
 var Server = thinky.createModel("Server", {
     id: String,
     hostname: String,
-//    vms: Array
+    //    vms: Array
 });
 
 // You can also add constraints on the schema
@@ -15,7 +14,17 @@ var VM = thinky.createModel("VM", {
     id: type.string(),
     hostname: type.string().min(5),
     ctid: type.string().min(5),
+    created: type.boolean(),
+    running: type.boolean(),
+    createdAt: type.date(),
+    createdLog: type.string(),
 });
+
+VM.ensureIndex('running');
+VM.ensureIndex('created');
+VM.ensureIndex('ctid');
+
+
 
 // Join the models
 VM.belongsTo(Server, "server", "idVM", "id");
@@ -23,25 +32,38 @@ VM.belongsTo(Server, "server", "idVM", "id");
 
 
 var server = new Server({
-hostname: 'enterprise',
+    hostname: 'enterprise',
 });
 
 // Create a new author
 var vm = new VM({
     hostname: "some.hostname",
     ctid: '123123',
+    created: false,
+    running: false,
 });
 
 // Join the documents
 vm.server = server;
 
 
-vm.saveAll().then(function(result) {
-console.log(result);
-if(result.id)
-    VM.get(result.id).getJoin().run().then(function(result) {
-if(result)
-        console.log(pj.render(result));
+//if (false)
+    vm.saveAll().then(function(result) {
+        console.log(result);
+        if (result.id)
+            VM.get(result.id).getJoin().run().then(function(result) {
+                if (result)
+                    console.log(pj.render(result));
+            });
     });
-});
 
+VM.filter({
+    created: true,
+}).run().then(function(result) {
+    console.log(pj.render(result));
+}).error(console.log);
+VM.orderBy({
+    index: "created"
+}).run().then(function(result) {
+    console.log(pj.render(result));
+}).error(console.log);
